7b1cbb472f23a61826d2c6310d71a0176085a0a7
Moriafly <62319450+Moriafly@users.noreply.github.com>
2020-09-25 10:55:05 +0800

    2020年9月25日10:55:03

--- mqtt-client/src/com/shou/mqtt/client/Client.kt
+++ mqtt-client/src/com/shou/mqtt/client/Client.kt
@@ -1,10 +1,22 @@
 package com.shou.mqtt.client
 
+import org.fusesource.mqtt.client.MQTT
+
 /**
  * MQTT 客户端类
  * @author Wu Shihao
  * @since 2020/9/25
  */
 class Client {
+    private val mqtt = MQTT()
 
+    /**
+     * 初始化 mqtt
+     */
+    fun initMqtt() {
+        mqtt.apply {
+            setHost("localhost", 1883) // 设置端口
+        }
+
+    }
 }
